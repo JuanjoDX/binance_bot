fc715f3ec6987433284d920ae467e1189d6eff45
JuanjoDX <juanjoposada182@hotmail.com>
2023-09-03 11:11:31 -0500

    se suben los cambios de Auto y ajuste en Monitoreo Activo

--- src/Monitoreo_pos_act.py
+++ src/Monitoreo_pos_act.py
@@ -9,86 +9,79 @@ apikey = 'o4xp0nX8Nr3RsQIAQDBs7ZZivwpoLHPZsDQU48dmWX8heBKpSgPOS0M9NZwHHbEP'
 secret = 'OwjJSwLt0szX7qq62Xqd7evsc345eCLCTrYIVTdIND4HORYJ4DDz7lmGnDGCMiwq'
 client = Client(apikey, secret)
 
-### Monitoreo Constante
+### Ver Operaciones Abiertas
 while True:
+    try:
+        res = client.futures_position_information()
+        target_coin = None
+        for position in res[::-1]:
+            if float(position['positionAmt']) != 0:
+                target_coin = position
+                break
 
-    ### Ver Operaciones Abiertas
-    while True:
-        try:
-            res = client.futures_position_information()
-            target_coin = {'symbol' : 'abc'}
-            for position in res[::-1]:
-                if float(position['positionAmt']) != 0:
-                    target_coin = position
-                    break
-            break
+    except BinanceAPIException as e:
+        # Espera 0.5 segundos antes de intentar nuevamente
+        time.sleep(0.5)
 
-        except BinanceAPIException as e:
-            # Espera 0.5 segundos antes de intentar nuevamente
-            time.sleep(0.5)
+    ### Monitoreo Constante
+    if target_coin != None:
+        ### Viendo Take Profit
+        while True:
+            try:
+                res = client.futures_get_open_orders(symbol = target_coin["symbol"])
+                for position in res[::-1]:
+                    if (position['type']) == "LIMIT":
+                        pos_tp = position
+                        break
+                break
+            except BinanceAPIException as e:
+                # Espera 0.5 segundos antes de intentar nuevamente
+                time.sleep(0.5)
 
-
-    ### Viendo Take Profit
-    while True:
-        try:
-            res = client.futures_get_open_orders(symbol = target_coin["symbol"])
-            pos_tp = {'symbol' : 'abc'}
-            for position in res[::-1]:
-                if (position['type']) == "LIMIT":
-                    pos_tp = position
-                    break
-            break
-        except BinanceAPIException as e:
-            # Espera 0.5 segundos antes de intentar nuevamente
-            time.sleep(0.5)
-
-
-    ### Viendo Stop Lost
-    while True:
-        try:
-            res = client.futures_get_open_orders(symbol = target_coin["symbol"])
-            pos_sl = {'symbol' : 'abc'}
-            for position in res[::-1]:
-                if (position['type']) == "STOP_MARKET":
-                    pos_sl = position
-                    break
-            break
-        except BinanceAPIException as e:
-            # Espera 0.5 segundos antes de intentar nuevamente
-            time.sleep(0.5)
-    
-    ### Tipo de Compra
-    aux = None
-    if pos_tp["side"] == "SELL":
-        aux = "LONG"
-    else:
-        aux = "SHORT"
-
-    ### Color Fondo
-    blue_background = "\033[44m"
-    yellow_background = "\033[43m"
-    reset_style = "\033[0m"
-
-    while True:
-        decimas = len(pos_tp["price"].split(".")[1])
-        try:
-            precio_entrada = str(round(float(client.futures_position_information(symbol = target_coin["symbol"])[0]['entryPrice']),decimas))
-            break
+        ### Viendo Stop Lost
+        while True:
+            try:
+                res = client.futures_get_open_orders(symbol = target_coin["symbol"])
+                for position in res[::-1]:
+                    if (position['type']) == "STOP_MARKET":
+                        pos_sl = position
+                        break
+                break
+            except BinanceAPIException as e:
+                # Espera 0.5 segundos antes de intentar nuevamente
+                time.sleep(0.5)
         
-        except BinanceAPIException as e:
-            # Espera 0.5 segundos antes de intentar nuevamente
-            time.sleep(0.5)
+        ### Tipo de Compra
+        aux = None
+        if pos_tp["side"] == "SELL":
+            aux = "LONG"
+        else:
+            aux = "SHORT"
 
-    ### Defiendo colores de fondo para el precio actual
-    if aux == "SHORT":
-        red_background = "\033[42m"
-        green_background= "\033[41m"
-    else:
-        green_background = "\033[42m"
-        red_background = "\033[41m"
+        ### Color Fondo
+        blue_background = "\033[44m"
+        yellow_background = "\033[43m"
+        reset_style = "\033[0m"
 
-    ### Monitoreo Continuo
-    if target_coin["symbol"] == target_coin["symbol"]:
+        while True:
+            decimas = len(pos_tp["price"].split(".")[1])
+            try:
+                precio_entrada = str(round(float(client.futures_position_information(symbol = target_coin["symbol"])[0]['entryPrice']),decimas))
+                break
+            
+            except BinanceAPIException as e:
+                # Espera 0.5 segundos antes de intentar nuevamente
+                time.sleep(0.5)
+
+        ### Defiendo colores de fondo para el precio actual
+        if aux == "SHORT":
+            red_background = "\033[42m"
+            green_background= "\033[41m"
+        else:
+            green_background = "\033[42m"
+            red_background = "\033[41m"
+
+        ### Monitoreo Continuo
         print("Moneda:", target_coin["symbol"])
         print("Tipo:" , aux)
         
@@ -112,26 +105,10 @@ while True:
 
         print(fecha)
         print("__________________________")
-        time.sleep(10)
+        time.sleep(10)         
     else:
         print("EN ESPERA")
-        
-        aux = None
-        if pos_tp["side"] == "SELL":
-            aux = "LONG"
-        else:
-            aux = "SHORT"
-        
-        print("Moneda:", pos_tp["symbol"])
-        print("Tipo:" , aux)
-
-        res = client.futures_symbol_ticker(symbol = pos_tp["symbol"])
         fecha = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        precio = pos_tp["price"]
-
-        print("Precio Actual:  ", res["price"])
-        print("Precio Entrada: ", precio)
-
         print(fecha)
         print("__________________________")
         time.sleep(5)
